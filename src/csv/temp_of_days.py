0f23be042495d22b6862329ca9b75a99c6b090c4
Jannah Hussain <jannahhussain@unicorn.local>
2021-03-27 12:37:29 +0000

    check in

--- src/csv/temp_of_days.py
+++ src/csv/temp_of_days.py
@@ -5,11 +5,8 @@ with open('days_and_temp.csv') as temp_file:
     csv_reader = csv.reader(temp_file, delimiter=",")
 
     current_position = 1
-
+    header = 1
     for line_array in csv_reader:
-        build_record = tc.do_build_record(line_array, current_position)
-
-        if build_record != "":
-            print(build_record)
-
+        build_data = tc.do_build_data(current_position, line_array, header)
+            print(line_array)
     current_position += 1
--- src/csv/temperature_calculations.py
+++ src/csv/temperature_calculations.py
@@ -4,18 +4,8 @@ def temperature(morning, afternoon, evening, night):
     return highest and lowest
 
 
-def do_build_record(line_array, current_position):
-    build_record = ""
+def do_build_record(line_array, current_position, header):
+    if current_position > 1:
+        return line_array
 
-    if current_position <= 0:
-        day = line_array[0]
-        morning = line_array[1]
-        afternoon = line_array[2]
-        evening = line_array[3]
-        night = line_array[4]
-
-        temp = temperature(day, morning, afternoon, evening, night)
-        build_record += day + " high of " + str(temp) + "low of " + str(temp)
-
-    return build_record
 
