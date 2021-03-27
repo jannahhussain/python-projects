0004f54ea6e3f3004f94455ba7719e3751d5bcfd
Jannah Hussain <jannahhussain@unicorn.local>
2021-03-27 12:50:08 +0000

    correction

--- src/csv/temp_of_days.py
+++ src/csv/temp_of_days.py
@@ -8,6 +8,6 @@ with open('days_and_temp.csv') as temp_file:
     header = 1
     for line_array in csv_reader:
         build_data = tc.do_build_record(line_array, current_position, header)
-        print(line_array)
+        print(build_data)
 
         current_position += 1
