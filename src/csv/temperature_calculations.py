a6fcd169608b9264ea9c7b7db7fbe7e1a2bb8ec4
Jannah Hussain <jannahhussain@unicorn.local>
2021-04-03 14:34:00 +0100

    cleanup

--- src/csv/temperature_calculations.py
+++ src/csv/temperature_calculations.py
@@ -9,22 +9,24 @@ def do_build_data(line_array, current_position, header):
         night = int(line_array[4])
 
         highest = max(morning, afternoon, evening, night)
-        position = line_array.index(str(highest))
-        highest_period = header[position]
+        highest_period = get_highest_period(highest, line_array, header)
 
         lowest = min(morning, afternoon, evening, night)
-        position = line_array.index(str(lowest))
-        lowest_period = header[position]
+        lowest_period = get_lowest_period(lowest, line_array, header)
 
-        build_data += day + ": highest temp = " + str(highest) + " the period is: " + highest_period + \
-                      ", lowest temp = " + str(lowest) + " the period is: " + lowest_period
+        build_data += day + ": highest temp = " + str(highest) + " - the period is: " + highest_period + \
+                      ", lowest temp = " + str(lowest) + " - the period is: " + lowest_period
 
         return build_data
 
 
 def get_highest_period(highest, line_array, header):
-    pass
+    position = line_array.index(str(highest))
+    highest_period = header[position]
+    return highest_period
 
 
 def get_lowest_period(lowest, line_array, header):
-    pass
+    position = line_array.index(str(lowest))
+    lowest_period = header[position]
+    return lowest_period
