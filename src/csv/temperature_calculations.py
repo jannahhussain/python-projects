d8a5478e591a57d9300a27c644cc7674d142fc5c
lomanhussain <loman.hussain@blackcatsolutions.com>
2021-04-03 15:06:03 +0100

    DRY - we ded nit need 2 functions that do the same thing

--- src/csv/temperature_calculations.py
+++ src/csv/temperature_calculations.py
@@ -9,10 +9,10 @@ def do_build_data(line_array, current_position, header):
         night = int(line_array[4])
 
         highest = max(morning, afternoon, evening, night)
-        highest_period = get_highest_period(highest, line_array, header)
+        highest_period = get_period(highest, line_array, header)
 
         lowest = min(morning, afternoon, evening, night)
-        lowest_period = get_lowest_period(lowest, line_array, header)
+        lowest_period = get_period(lowest, line_array, header)
 
         build_data += day + ": highest temp = " + str(highest) + " - the period is: " + highest_period + \
                       ", lowest temp = " + str(lowest) + " - the period is: " + lowest_period
@@ -20,13 +20,7 @@ def do_build_data(line_array, current_position, header):
         return build_data
 
 
-def get_highest_period(highest, line_array, header):
-    position = line_array.index(str(highest))
-    highest_period = header[position]
-    return highest_period
-
-
-def get_lowest_period(lowest, line_array, header):
-    position = line_array.index(str(lowest))
-    lowest_period = header[position]
-    return lowest_period
+def get_period(value, line_array, header):
+    position = line_array.index(str(value))
+    period = header[position]
+    return period
