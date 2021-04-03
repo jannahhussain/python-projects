9c0c40b52a9f89f6a52df73f272ba1e6d323c745
jannahhussain <79409666+jannahhussain@users.noreply.github.com>
2021-04-03 14:37:52 +0100

    Feature/calculate period (#5)
    * added in the period
    
    * added functions
    
    * quick tidy up
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    Co-authored-by: Jannah Hussain <jannahhussain@unicorn.local>

--- src/csv/temp_of_days.py
+++ src/csv/temp_of_days.py
@@ -5,8 +5,12 @@ with open('days_and_temp.csv') as temp_file:
     csv_reader = csv.reader(temp_file, delimiter=",")
 
     current_position = 1
+    header = []
     for line_array in csv_reader:
-        build_data = tc.do_build_data(line_array, current_position)
+        if current_position == 1:
+            header = line_array
+
+        build_data = tc.do_build_data(line_array, current_position, header)
         if build_data is not None:
             print(build_data)
 
