b57324a81082ca55a93aa01a597a31fb01b13146
Jannah Hussain <jannahhussain@unicorn.local>
2021-04-03 14:27:12 +0100

    added functions

--- src/csv/temperature_calculations.py
+++ src/csv/temperature_calculations.py
@@ -1,6 +1,7 @@
 def do_build_data(line_array, current_position, header):
     build_data = ""
 
+
     if current_position > 1:
         day = line_array[0]
         morning = int(line_array[1])
@@ -20,4 +21,10 @@ def do_build_data(line_array, current_position, header):
                       ", lowest temp = " + str(lowest) + " the period is: " + lowest_period
 
         return build_data
+
+def get_highest_period(highest, line_array, header):
+    pass
+
+def get_lowest_period(lowest, line_array, header):
+    pass
 
