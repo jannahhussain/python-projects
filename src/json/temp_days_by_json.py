eac9e638651099f2fdc597e9b64926b06892381d
lomanhussain <loman.hussain@blackcatsolutions.com>
2021-07-03 14:17:00 +0100

    added some pretty formatting and printing out data

--- src/json/temp_days_by_json.py
+++ src/json/temp_days_by_json.py
@@ -2,3 +2,14 @@ import json
 
 with open('days_and_temp.json') as temp_file:
     data = json.load(temp_file)
+    json_formatted_str = json.dumps(data, indent=2)
+    print(json_formatted_str)
+
+    '''
+    in python when ever you want to access the json object properties use the brackets operator
+    '''
+    data_for_week_array = data["tempForWeek"]
+    data_for_monday = data_for_week_array[0]
+
+    print(data_for_monday["morning"])
+    print(data_for_monday["afternoon"])
